627cc2fc90244d799bdff4b9e9579c8bc3bfe58b
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2021-03-12 07:12:40 +0300

    I tried to create the Add () method of the Dictionary Class myself.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,V>
+    {
+        T[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new V[0];
+        }
+        public void Add(T key, V value)
+        {
+            T[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new T[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void List()//ikisinin bir arada listelenmesi
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] +" : "+values[i]);
+            }
+        }
+
+        public T[] Keys // Sadece keylerin listelenmesi
+        {
+            get
+            {
+                return keys;
+            }
+        }
+
+        public V[] Values // sadece değerlerin listelenmesi
+        {
+            get
+            {
+                return values;
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "A");
+            myDictionary.Add(2, "B");
+            myDictionary.Add(3, "C");
+            myDictionary.Add(4, "D");
+            myDictionary.Add(5, "E");
+            myDictionary.Add(6, "F");
+            myDictionary.Add(7, "G");
+            myDictionary.Add(8, "H");
+            myDictionary.Add(9, "I");
+            myDictionary.Add(10, "J");
+
+            myDictionary.List();                         //ikisinin bir arada listelenmesi
+
+            foreach (var item in myDictionary.Keys)      // Sadece keylerin listelenmesi
+            {
+                Console.WriteLine(item); 
+            }
+
+            foreach (var item in myDictionary.Values)    // sadece değerlerin listelenmesi
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
